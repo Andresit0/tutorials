9f46aa443fe2bad0a65462471698b3707ba29f60
Andresit0 <rvandres001@gmail.com>
2022-01-02 11:56:17 -0500

    postgres division

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6e1873799c14437f0f6d796bec850e16",
-"index.html": "96e2fe75c7cf2feaec28ffeed12e28ac",
+"index.html": "9daea166d98b124e066d236613c3eb57",
 "/": "73be3445adc8d49407f4b36e21d3236c",
-"main.dart.js": "590514628a7e0cc02de5e97a88c73fa6",
+"main.dart.js": "baa71569d33b372b9b41a0d0b8672a0a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
