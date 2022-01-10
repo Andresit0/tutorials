cb049cee41d7c6098322f80f001c17b86e97d90e
Andresit0 <rvandres001@gmail.com>
2022-01-09 22:26:13 -0500

    update font name

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6e1873799c14437f0f6d796bec850e16",
-"index.html": "6aaaae793c82d355feeead54f4079e17",
+"index.html": "8e23b321a884db9a7dda3a025e3a38f4",
 "/": "73be3445adc8d49407f4b36e21d3236c",
-"main.dart.js": "dd5402e11d3c4553117c0d102583a4bc",
+"main.dart.js": "f1a37392eff7eb7cead0590f2b4465af",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
